f33fbbd3edf86c6144b896adc2f277a96c40878c
Livia Fortunato <livia.fortunato@academico.ifpb.edu.br>
2022-03-08 16:22:16 -0300

    NavBar

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+    return(
+        <header>
+      <nav className="container">
+        <div className="dsmovienav-content">
+        <h1>DSMovie</h1>
+        <a href="https://github.com/liviafort">
+        <div className="dsmovie-container">
+          <GithubIcon />
+          <p className="dsmovie-contactlink">/liviafort</p>
+        </div>
+        </a>
+        </div>
+      </nav>
+    </header>
+    )
+
+}
+
+export default Navbar;
