028a0538279c1217a6342bbe45cc1fda5411f290
Livia Fortunato <livia.fortunato@academico.ifpb.edu.br>
2022-03-08 17:06:53 -0300

    Pagination Layout

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,11 @@
+import Pagination from "components/pagination";
+
 function Listing(){
 
     return(
-        <h1>Página de Listagem;</h1>
+        <Pagination/>
 
-    )
+    );
 }
 
 export default Listing
